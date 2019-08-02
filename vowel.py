4985854b355a3e2cc2ccf3fc2c41a6ea87682187
Manimsn <48318047+Manimsn@users.noreply.github.com>
2019-08-02 16:35:21 +0530

    create vowel.py
    vowel or not

---
+++ vowel.py
@@ -0,0 +1,6 @@
+y = a
+if( y == 'a' or y == 'e' or y == 'i' or y == 'o' or y == 'u' or
+    y == 'A' or y == 'E' or y == 'I' or y == 'O' or y == 'U'):
+    print("valid")
+else:
+    print("invalid")
